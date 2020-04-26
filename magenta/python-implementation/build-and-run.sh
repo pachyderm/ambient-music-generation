be50f0bcec40141219059bca2ecf34501107ead8
Kevin Scott <kevin@hitheory.com>
2020-04-26 18:18:58 -0400

    Add working transcribe code

--- magenta/python-implementation/build-and-run.sh
+++ magenta/python-implementation/build-and-run.sh
@@ -3,7 +3,9 @@
 
 err=0
 trap 'err=1' ERR
-docker build -t onsets-and-frames .
+docker build -t onsets-and-frames -f jupyter.Dockerfile .
+
+
 test $err = 0 # Return non-zero if any command failed
 
 if [ $err == 0 ]
@@ -12,7 +14,12 @@ then
   # docker run --name onsets-and-frames -p 8888:8888 -v $(pwd)/src:/code/src -v $(pwd)/checkpoints:/checkpoints -v $(pwd)/samples:/samples --rm -it onsets-and-frames
 
   # To run training out of the box:
-  docker run --name onsets-and-frames -v $(pwd)/src:/code/src -v $(pwd)/checkpoints:/checkpoints -v $(pwd)/samples:/samples --rm -it onsets-and-frames ./src/transcribe
+  # docker run --name onsets-and-frames -v $(pwd)/src:/code/src -v $(pwd)/checkpoints:/checkpoints -v $(pwd)/samples:/samples --rm -it onsets-and-frames ./src/transcribe
+
+    docker run --name onsets-and-frames --rm -it -p 8889:8888 --runtime nvidia \
+    --shm-size=1g -e NVIDIA_VISIBLE_DEVICES=0,1 -v $(pwd)/src:/notebooks/src -v \
+    $(pwd)/audio:/pfs/dev-audio-processed-wav -v $(pwd)/out:/pfs/out \
+    onsets-and-frames python3 /notebooks/src/transcribe.py
 else
   echo 'error building docker file'
 fi
