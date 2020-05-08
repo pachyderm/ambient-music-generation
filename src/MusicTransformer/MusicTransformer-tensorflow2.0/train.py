ad75964c719afce457d6228d3dd334fa22e3f235
Kevin Scott <kevin@hitheory.com>
2020-05-08 08:51:15 -0400

    Add working code

--- src/MusicTransformer/MusicTransformer-tensorflow2.0/train.py
+++ src/MusicTransformer/MusicTransformer-tensorflow2.0/train.py
@@ -8,6 +8,9 @@ import utils
 import argparse
 import datetime
 import sys
+import os
+#os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 tf.executing_eagerly()
 
@@ -41,7 +44,7 @@ num_layer = args.num_layers
 
 
 # load data
-dataset = Data('dataset/processed')
+dataset = Data('/data/classic_piano_preprocessed')
 print(dataset)
 
 
