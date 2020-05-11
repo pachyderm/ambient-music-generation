0ed583555519fa58a680c79e3d773dcc6f2751ec
Kevin Scott <kevin@hitheory.com>
2020-05-11 08:11:30 -0400

    Add latest transformer code

--- src/MusicTransformer/src/train_script.sh
+++ src/MusicTransformer/src/train_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python3 /tf/MusicTransformer-tensorflow2.0/train.py \
-  --epochs=200 --save_path=/save --max_seq=1024 \
+  --epochs=100 --save_path=/save --max_seq=1024 \
   --pickle_dir=/pickle \
   --batch_size=1 --l_r=0.0001
